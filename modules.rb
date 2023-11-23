891e795910d9336c5eba278b441884d8956d8b75
randhawah <harman_randhawa@aol.com>
2023-11-23 15:15:06 -0500

    when to use class vs interface inheritance comment

--- modules.rb
+++ modules.rb
@@ -1,3 +1,11 @@
+#module inheritance is also called interface inheritance
+# When to use class vs Interface Inheritance?
+# 1. You can only subclass (class inheritance) from one class. You can mix in as many modules (interface inheritance) as you'd like. 
+# 2. If there's an "is-a" relationship, class inheritance is usually the correct choice. If there's a "has-a" relationship, 
+#       interface inheritance is generally a better choice. For example, a dog "is an" animal and it "has an" ability to swim. 
+# 3. You cannot instantiate modules. In other words, objects cannot be created from modules.
+
+
 module Swimmable
     def swim
     "I'm swimming!"
