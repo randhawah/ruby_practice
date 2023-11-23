c9396da7e98bd8cfc6b800974d1443ff8fc30d5b
randhawah <harman_randhawa@aol.com>
2023-11-23 14:59:18 -0500

    test commit to check commiting acount

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -14,3 +14,4 @@ sparky = GoodDog.new
 paws = Cat.new
 puts sparky.speak           # => Hello!!
 puts paws.speak             # => Hello!!
+#test commit to see if it VS Code is using the right acount to commit changes
