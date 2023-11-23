7774249dcfe05630a9dc6f0b0236fb5bb5549fe5
randhawah <harman_randhawa@aol.com>
2023-11-23 15:01:06 -0500

    class overriding the super class method

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -1,17 +1,26 @@
 class Animal
     def speak
-    "Hello!!"
+      "Hello!"
     end
-end
-
-class GoodDog < Animal
-end
-
-class Cat < Animal
-end
-
-sparky = GoodDog.new
-paws = Cat.new
-puts sparky.speak           # => Hello!!
-puts paws.speak             # => Hello!!
-#test commit to see if it VS Code is using the right acount to commit changes
+  end
+  
+  class GoodDog < Animal
+    attr_accessor :name
+  
+    def initialize(n)
+      self.name = n
+    end
+  
+    def speak
+      "#{self.name} says arf!"
+    end
+  end
+  
+  class Cat < Animal
+  end
+  
+  sparky = GoodDog.new("Sparky")
+  paws = Cat.new
+  
+  puts sparky.speak           # => Sparky says arf!
+  puts paws.speak             # => Hello!
